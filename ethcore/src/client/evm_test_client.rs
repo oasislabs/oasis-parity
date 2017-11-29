79d35f66a83aac3cb7ee3bbe2c8e8270490c5763
Tomasz Drwięga <tomasz@parity.io>
2017-11-29 15:40:36 +0100

    Small changes to allow better re-use of EvmTestClient

--- ethcore/src/client/evm_test_client.rs
+++ ethcore/src/client/evm_test_client.rs
@@ -80,6 +80,15 @@ pub struct EvmTestClient<'a> {
 	spec: &'a spec::Spec,
 }
 
+impl<'a> fmt::Debug for EvmTestClient<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct("EvmTestClient")
+            .field("state", &self.state)
+            .field("spec", &self.spec.name)
+            .finish()
+    }
+}
+
 impl<'a> EvmTestClient<'a> {
 	/// Converts a json spec definition into spec.
 	pub fn spec_from_json(spec: &ForkSpec) -> Option<&'static spec::Spec> {
@@ -161,10 +170,19 @@ impl<'a> EvmTestClient<'a> {
 		Ok(state)
 	}
 
+	/// Return current state.
+	pub fn state(&self) -> &state::State<state_db::StateDB> {
+		&self.state
+	}
+
 	/// Execute the VM given ActionParams and tracer.
 	/// Returns amount of gas left and the output.
-	pub fn call<T: trace::VMTracer>(&mut self, params: ActionParams, vm_tracer: &mut T)
-		-> Result<FinalizationResult, EvmTestError>
+	pub fn call<T: trace::Tracer, V: trace::VMTracer>(
+		&mut self,
+		params: ActionParams,
+		tracer: &mut T,
+		vm_tracer: &mut V,
+	) -> Result<FinalizationResult, EvmTestError>
 	{
 		let genesis = self.spec.genesis_header();
 		let info = client::EnvInfo {
@@ -177,39 +195,39 @@ impl<'a> EvmTestClient<'a> {
 			gas_limit: *genesis.gas_limit(),
 		};
 		let mut substate = state::Substate::new();
-		let mut tracer = trace::NoopTracer;
 		let mut output = vec![];
 		let mut executive = executive::Executive::new(&mut self.state, &info, self.spec.engine.machine());
 		executive.call(
 			params,
 			&mut substate,
 			bytes::BytesRef::Flexible(&mut output),
-			&mut tracer,
+			tracer,
 			vm_tracer,
 		).map_err(EvmTestError::Evm)
 	}
 
 	/// Executes a SignedTransaction within context of the provided state and `EnvInfo`.
 	/// Returns the state root, gas left and the output.
-	pub fn transact<T: trace::VMTracer>(
+	pub fn transact<T: trace::Tracer, V: trace::VMTracer>(
 		&mut self,
 		env_info: &client::EnvInfo,
 		transaction: transaction::SignedTransaction,
-		vm_tracer: T,
-	) -> TransactResult<T::Output> {
+		tracer: T,
+		vm_tracer: V,
+	) -> TransactResult<T::Output, V::Output> {
 		let initial_gas = transaction.gas;
 		// Verify transaction
 		let is_ok = transaction.verify_basic(true, None, env_info.number >= self.spec.engine.params().eip86_transition);
 		if let Err(error) = is_ok {
 			return TransactResult::Err {
 				state_root: *self.state.root(),
-				error: error.into(),
+				error,
 			};
 		}
 
 		// Apply transaction
-		let tracer = trace::NoopTracer;
 		let result = self.state.apply_with_tracing(&env_info, self.spec.engine.machine(), &transaction, tracer, vm_tracer);
+		let scheme = self.spec.engine.machine().create_address_scheme(env_info.number);
 
 		match result {
 			Ok(result) => {
@@ -218,7 +236,13 @@ impl<'a> EvmTestClient<'a> {
 					state_root: *self.state.root(),
 					gas_left: initial_gas - result.receipt.gas_used,
 					output: result.output,
+					trace: result.trace,
 					vm_trace: result.vm_trace,
+					contract_address: if let transaction::Action::Create = transaction.action {
+						Some(executive::contract_address(scheme, &transaction.sender(), &transaction.nonce, &transaction.data).0)
+					} else {
+						None
+					}
 				}
 			},
 			Err(error) => TransactResult::Err {
@@ -230,7 +254,8 @@ impl<'a> EvmTestClient<'a> {
 }
 
 /// A result of applying transaction to the state.
-pub enum TransactResult<T> {
+#[derive(Debug)]
+pub enum TransactResult<T, V> {
 	/// Successful execution
 	Ok {
 		/// State root
@@ -239,8 +264,12 @@ pub enum TransactResult<T> {
 		gas_left: U256,
 		/// Output
 		output: Vec<u8>,
+		/// Traces
+		trace: Vec<T>,
 		/// VM Traces
-		vm_trace: Option<T>,
+		vm_trace: Option<V>,
+		/// Created contract address (if any)
+		contract_address: Option<H160>,
 	},
 	/// Transaction failed to run
 	Err {
