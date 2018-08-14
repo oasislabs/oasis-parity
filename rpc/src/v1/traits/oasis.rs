9e4129b7b93f9aa9e483feefc36adda3093e3c21
David Yan <david.yan@berkeley.edu>
2018-08-14 21:42:04 +0000

    Update Oasis trait function signatures

--- rpc/src/v1/traits/oasis.rs
+++ rpc/src/v1/traits/oasis.rs
@@ -1,5 +1,5 @@
 //! Eth rpc interface.
-use jsonrpc_core::{Result, BoxFuture};
+use jsonrpc_core:::Result;
 use jsonrpc_macros::Trailing;
 
 use v1::types::{H256, U256, Bytes};
@@ -8,10 +8,10 @@ build_rpc_trait! {
 	pub trait Oasis {
 		/// Request data from storage.
 		#[rpc(name = "oasis_requestBytes")]
-		fn request_bytes(&self, H256, U256, U256) -> Result<Bytes>;
+		fn request_bytes(&self, H256) -> Result<String>;
 
 		/// Store data in global storage.
 		#[rpc(name = "oasis_storeBytes")]
-		fn store_bytes(&self, Bytes, U256) -> Result<H256>;
+		fn store_bytes(&self, String, U64) -> Result<H256>;
 	}
 }
