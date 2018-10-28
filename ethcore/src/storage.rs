7648262e86fbc6535b94d93002bcf457908a34cc
Peter Gilbert <pgilbert@oasislabs.com>
2018-10-28 20:00:17 +0000

    ethcore: remove 'gateway' feature

--- ethcore/src/storage.rs
+++ ethcore/src/storage.rs
@@ -3,10 +3,7 @@ use vm::Result;
 
 pub trait Storage {
 	fn fetch_bytes(&self, key: &H256) -> Result<Vec<u8>>;
-	#[cfg(feature = "gateway")]
 	fn store_bytes(&self, bytes: &[u8]) -> Result<H256>;
-	#[cfg(not(feature = "gateway"))]
-	fn store_bytes(&mut self, bytes: &[u8]) -> Result<H256>;
 }
 
 pub struct NullStorage;
@@ -22,13 +19,7 @@ impl Storage for NullStorage {
 		unimplemented!();
 	}
 
-	#[cfg(feature = "gateway")]
 	fn store_bytes(&self, _bytes: &[u8]) -> Result<H256> {
-		unimplemented!();
-	}
-
-	#[cfg(not(feature = "gateway"))]
-	fn store_bytes(&mut self, _bytes: &[u8]) -> Result<H256> {
 		unimplemented!();
 	}
 }
