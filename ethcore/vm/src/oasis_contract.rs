5427f1f5b9507174eb92ce2a725d1393bbfe8e45
Peter Gilbert <pgilbert@oasislabs.com>
2019-02-16 17:30:12 +0000

    Allow only version 1 contract header

--- ethcore/vm/src/oasis_contract.rs
+++ ethcore/vm/src/oasis_contract.rs
@@ -42,6 +42,10 @@ impl OasisContract {
 			return Err("Invalid header version".to_string());
 		}
 		let header_version = BigEndian::read_u16(&data[..2]) as usize;
+		// only version 1 allowed
+		if header_version != 1 {
+			return Err("Invalid header version".to_string());
+		}
 		let data = &data[2..];
 
 		// read length (2 bytes, big-endian)
@@ -90,7 +94,7 @@ mod tests {
 	use super::*;
 	use elastic_array::ElasticArray128;
 
-	fn make_data_payload(json_str: String) -> Vec<u8> {
+	fn make_data_payload(version: usize, json_str: String) -> Vec<u8> {
 		// start with header prefix
 		let mut data = ElasticArray128::from_slice(&HEADER_PREFIX[..]);
 
@@ -98,15 +102,15 @@ mod tests {
 		let contents = json_str.into_bytes();
 
 		// header version
-		let mut version = [0u8; 2];
-		BigEndian::write_u16(&mut version, 1 as u16);
+		let mut header_version = [0u8; 2];
+		BigEndian::write_u16(&mut header_version, version as u16);
 
 		// header contents length
 		let mut length = [0u8; 2];
 		BigEndian::write_u16(&mut length, contents.len() as u16);
 
 		// append header length, version and contents
-		data.append_slice(&version);
+		data.append_slice(&header_version);
 		data.append_slice(&length);
 		data.append_slice(&contents);
 
@@ -118,7 +122,7 @@ mod tests {
 
 	#[test]
 	fn test_valid_header() {
-		let data = make_data_payload(json!({
+		let data = make_data_payload(1, json!({
 			"confidential": true,
 			"expiry": 1577836800,
 		}).to_string());
@@ -132,7 +136,7 @@ mod tests {
 
 	#[test]
 	fn test_confidential_only() {
-		let data = make_data_payload(json!({
+		let data = make_data_payload(1, json!({
 			"confidential": true,
 		}).to_string());
 
@@ -144,7 +148,7 @@ mod tests {
 
 	#[test]
 	fn test_expiry_only() {
-		let data = make_data_payload(json!({
+		let data = make_data_payload(1, json!({
 			"expiry": 1577836800,
 		}).to_string());
 
@@ -155,8 +159,19 @@ mod tests {
 	}
 
 	#[test]
+	fn test_invalid_version() {
+		let data = make_data_payload(2, json!({
+			"confidential": true,
+			"expiry": 1577836800,
+		}).to_string());
+
+		let result = OasisContract::from_code(&data);
+		assert!(result.is_err());
+	}
+
+	#[test]
 	fn test_invalid_key() {
-		let data = make_data_payload(json!({
+		let data = make_data_payload(1, json!({
 			"expiry": 1577836800,
 			"unknown": "something",
 			"blah": "blah",
@@ -168,7 +183,7 @@ mod tests {
 
 	#[test]
 	fn test_duplicate_key() {
-		let data = make_data_payload(
+		let data = make_data_payload(1,
 			"{\"expiry\":1577836800,\"confidential\":true,\"expiry\":1577836801}"
 			.to_string());
 
@@ -178,7 +193,7 @@ mod tests {
 
 	#[test]
 	fn test_invalid_json() {
-		let data = make_data_payload(json!("expiry").to_string());
+		let data = make_data_payload(1, json!("expiry").to_string());
 
 		let result = OasisContract::from_code(&data);
 		assert!(result.is_err());
@@ -213,7 +228,7 @@ mod tests {
 	#[test]
 	fn test_invalid_values() {
 		// create a header with incorrect confidential type
-		let invalid_confidential = make_data_payload(json!({
+		let invalid_confidential = make_data_payload(1, json!({
 			"confidential": "true",
 			"expiry": 1577836800,
 		}).to_string());
@@ -222,7 +237,7 @@ mod tests {
 		assert!(result.is_err());
 
 		// create a header with incorrect expiry type
-		let invalid_expiry = make_data_payload(json!({
+		let invalid_expiry = make_data_payload(1, json!({
 			"confidential": true,
 			"expiry": "1577836800",
 		}).to_string());
